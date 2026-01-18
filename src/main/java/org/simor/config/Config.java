435ab0c10f9958860edf83737bca3904b34d75b5
simo_r <russo96.simone@gmail.com>
2026-01-18 12:13:25 +0100

    fix: use interface type

--- src/main/java/org/simor/config/Config.java
+++ src/main/java/org/simor/config/Config.java
@@ -1,5 +1,6 @@
 package org.simor.config;
 
+import org.simor.adapter.client.TranslationClient;
 import org.simor.adapter.client.TranslationRestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,14 +10,14 @@ import org.springframework.context.annotation.Configuration;
 public class Config {
 
     @Bean("shakespeareTranslation")
-    public TranslationRestClient shakespeareTranslationRestClient(
+    public TranslationClient shakespeareTranslationRestClient(
             @Value("${rest-client.shakespeare-translation.base-url}") String baseUrl,
             @Value("${rest-client.shakespeare-translation.path}") String path) {
         return new TranslationRestClient(baseUrl, path);
     }
 
     @Bean("yodaTranslation")
-    public TranslationRestClient yodaTranslationRestClient(
+    public TranslationClient yodaTranslationRestClient(
             @Value("${rest-client.yoda-translation.base-url}") String baseUrl,
             @Value("${rest-client.yoda-translation.path}") String path) {
         return new TranslationRestClient(baseUrl, path);
